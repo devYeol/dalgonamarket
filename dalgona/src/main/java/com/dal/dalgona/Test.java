658a1212ed11ce8b57b38fb92db644d5591bc728
RyuWonHee <dnjs1@DESKTOP-6M4H485>
2022-07-28 12:49:08 +0900

    firstproject

---
+++ dalgona/src/main/java/com/dal/dalgona/Test.java
@@ -0,0 +1,14 @@
+package com.dal.dalgona;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+}
