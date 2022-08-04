f22606de3d5d556b54aa3d0f893894a55431de56
user <user@DESKTOP-OPIEU5B>
2022-08-04 12:16:10 +0900

    워킹 패키지에 기존 클래스 이동

--- dalgona/src/main/java/com/dal/dalgona/working/controller/MswController.java
+++ dalgona/src/main/java/com/dal/dalgona/working/controller/MswController.java
@@ -1,15 +1,14 @@
-package com.dal.dalgona.mswcontroller;
+package com.dal.dalgona.working.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 public class MswController {
 
-	
 	@RequestMapping("/msw/msw")
 	public String msw() {
 		return "/msw/msw";
 	}
+	
 }
