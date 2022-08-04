762970313ab73407fe4c45b7b7bb2513f74282b4
user <user@DESKTOP-OPIEU5B>
2022-08-04 15:42:28 +0900

    msw => product folder 이동 productList로 이름 변경

--- dalgona/src/main/java/com/dal/dalgona/working/controller/MswController.java
+++ dalgona/src/main/java/com/dal/dalgona/working/controller/MswController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MswController {
 
-	@RequestMapping("/msw/msw")
+	@RequestMapping("/product/productList")
 	public String msw() {
-		return "/msw/msw";
+		return "/product/productList";
 	}
 	
 }
