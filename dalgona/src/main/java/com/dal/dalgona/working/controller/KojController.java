8b9a765ae88e4f65396ad63935900dc2da2febfd
kwons <kwons@DESKTOP-FV81F8F>
2022-08-05 17:53:14 +0900

    수정중

--- dalgona/src/main/java/com/dal/dalgona/working/controller/KojController.java
+++ dalgona/src/main/java/com/dal/dalgona/working/controller/KojController.java
@@ -1,27 +1,26 @@
-//package com.dal.dalgona.working.controller;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import com.dal.dalgona.common.model.vo.Product;
-//import com.dal.dalgona.working.model.service.KojService;
-//
-//@Controller
-//public class KojController {
-//	
-//	@Autowired
-//	private KojService service;
-//	
-//	@RequestMapping("/product/productDetail")	
-//	public String storeDetail(@PathVariable String productCode, @PathVariable String optionCode,Model model ) {
-//		
-//		//List<Product> proList=service.productList(productCode,optionCode);
-//		return "product/productDetail";
-//	}
-//
-//}
+package com.dal.dalgona.working.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dal.dalgona.working.model.service.KojService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class KojController {
+	
+	@Autowired
+	private KojService service;
+	
+	@RequestMapping("/product/productDetail/{productCode}")	
+	public String storeDetail(@PathVariable long productCode, Model model) {
+		//service.selectProduct(productCode);
+		return "product/productDetail";
+	}
+
+}
--- dalgona/src/main/java/com/dal/dalgona/working/model/service/KojService.java
+++ dalgona/src/main/java/com/dal/dalgona/working/model/service/KojService.java
@@ -1,5 +1,8 @@
 package com.dal.dalgona.working.model.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class KojService {
 
 }
