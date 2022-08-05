de5171ef1bba79b3cbc81da810ae179507464aea
epike <epike@BOOK-68LALPDDBA>
2022-08-05 17:54:10 +0900

    상품관리 오류해결중

--- dalgona/src/main/java/com/dal/dalgona/admin/controller/adminController.java
+++ dalgona/src/main/java/com/dal/dalgona/admin/controller/adminController.java
@@ -1,10 +1,16 @@
 package com.dal.dalgona.admin.controller;
 
-import java.util.List;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dal.dalgona.admin.model.service.adminService;
@@ -16,22 +22,37 @@ public class adminController {
 	@Autowired
 	adminService service;
 	
+	// 관리자 메인 이동
 	@RequestMapping("adminMain.do")
 	public String adminMain() {
 		return "admin/adminMain";
 	}
 	
+	// 상품등록 페이지 이동
 	@RequestMapping("adminEnrollProduct.do")
 	public String adminEnrollProduct() {
 		return "admin/adminEnrollProduct";
 	}
 	
+	// 상품관리 페이지 이동
 	@RequestMapping("adminManageProduct.do")
 	public ModelAndView adminManageProduct(ModelAndView mv) {
-		List<Product> list=service.selectProducts();
+		PageRequest pageRequest = PageRequest.of(0, 5);
+		Page<Product> list=service.selectProducts(pageRequest);
+		mv.addObject("products",list);
+		mv.setViewName("admin/adminManageProduct");
 		return mv;
 	}
 	
+	// 상품관리 - 삭제기능
+	@RequestMapping("adminDeleteProduct.do")
+	@ResponseBody
+	public boolean deleteProduct(long productCode, HttpServletResponse response) throws IOException{
+		Long result=service.deleteByProductCode(productCode);
+		return result>0;
+	}
+	
+	// 주문관리 페이지 이동
 	@RequestMapping("adminManageOrder.do")
 	public String adminManageOrder() {
 		return "admin/adminManageOrder";
--- dalgona/src/main/java/com/dal/dalgona/admin/model/dao/adminDao.java
+++ dalgona/src/main/java/com/dal/dalgona/admin/model/dao/adminDao.java
@@ -1,13 +1,18 @@
 package com.dal.dalgona.admin.model.dao;
 
-import java.util.List;
+import javax.transaction.Transactional;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dal.dalgona.common.model.vo.Product;
 
 @Repository
-public interface adminDao extends JpaRepository<Product, Integer> {
-	List<Product> findAll();
+public interface adminDao extends JpaRepository<Product, Long> {
+	Page<Product> findAll(Pageable pagable);
+	
+	@Transactional
+	Long deleteByProductCode(long productCode);
 }
