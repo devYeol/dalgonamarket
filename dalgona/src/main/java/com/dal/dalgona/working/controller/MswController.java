05245e3d08a83d4bf151ad3a53a0a3eab3bc3356
user <user@DESKTOP-OPIEU5B>
2022-08-05 09:37:39 +0900

    마이페이지 작업중

--- dalgona/src/main/java/com/dal/dalgona/working/controller/MswController.java
+++ dalgona/src/main/java/com/dal/dalgona/working/controller/MswController.java
@@ -4,15 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/member")
 public class MswController {
 
-	@RequestMapping("/product/productList")
-	public String productList() {
-		return "/product/productList";
-	}
 	@RequestMapping("/member/mypage/mypageMain")
 	public String mypageMain() {
 		return "/member/mypage/mypageMain";
 	}
 	
+}
+
+
+
+
+@Controller
+class Msw2Controller {
+	@RequestMapping("/product/productList")
+	public String productList() {
+		return "/product/productList";
+	}
 }
