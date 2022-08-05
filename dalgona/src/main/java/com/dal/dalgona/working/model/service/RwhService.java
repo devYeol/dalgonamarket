959e46d7f1338ff8f7893de5f3bec49279e5a7b1
RyuWonHee <dnjs1@DESKTOP-6M4H485>
2022-08-05 17:52:00 +0900

    상품등록 하는중

--- dalgona/src/main/java/com/dal/dalgona/working/model/service/RwhService.java
+++ dalgona/src/main/java/com/dal/dalgona/working/model/service/RwhService.java
@@ -1,8 +1,25 @@
 package com.dal.dalgona.working.model.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.dal.dalgona.common.model.vo.Category;
+import com.dal.dalgona.common.model.vo.Member;
+import com.dal.dalgona.common.model.vo.Product;
+import com.dal.dalgona.common.model.vo.ProductOption;
 
 @Service
 public interface RwhService {
+	
+	
+	List<ProductOption> insertProduct(List<ProductOption> options); 
 
+	Product insertProduct(Product p);
+	
+	Category selectCategory(String categoryName);
+	
+	Category insertCategory(Category cate);
+	
+	
 }
