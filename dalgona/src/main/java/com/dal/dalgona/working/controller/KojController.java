9eb6ffcd7c0936d1c043e9813e32b07680d4fa94
kwons <kwons@192.168.10.4>
2022-08-10 10:17:15 +0900

    ㅇㅇㅇ

--- dalgona/src/main/java/com/dal/dalgona/working/controller/KojController.java
+++ dalgona/src/main/java/com/dal/dalgona/working/controller/KojController.java
@@ -40,46 +40,45 @@ public class KojController {
 	}
 	
 	
-//	@RequestMapping("/review/reviewWrite.do")
-//	public String reviewWrite(Review r, MultipartFile upFile,Model m,HttpServletRequest rs) {
-//			
-//		String path=rs.getServletContext().getRealPath("/resources/upload/review/");
-//		File uploadDir=new File(path);
-//		
-//		if(!uploadDir.exists()) uploadDir.mkdirs();
-//		
-//		if(!upFile.isEmpty()) {
-//			//리네임드처리하기
-//			String originalFilename=upFile.getOriginalFilename();
-//			String ext=originalFilename.substring(originalFilename.lastIndexOf("."));
-//			//리네임 명칭을 정할값 설정
-//			SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMdd_HHmmssSSS");
-//			int rndNum=(int)(Math.random()*10000);
-//			String rename=sdf.format(System.currentTimeMillis())+"_"+rndNum+ext;
-//						
-//			//업로드처리하기
-//			try {
-//				upFile.transferTo(new File(path+rename));
-//			}catch(IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		
-//		String msg="";
-//		String loc="";
-//		try {
-//			service.reviewWrite(r);
-//			msg="게시글입력성공";	
-//			loc="/board/boardList.do";
-//		}catch(RuntimeException e) {
-//			msg="게시글입력실패";	
-//			loc="/board/boardWrite.do";			
-//		}
-//		
-//		m.addAttribute("msg",msg);
-//		m.addAttribute("loc",loc);
-//	
-//		return "common/msg";
-//	}
+	@RequestMapping("/review/reviewWrite.do")
+	public String reviewWrite(Review r, MultipartFile upFile,Model m,HttpServletRequest rs) {
+			
+		String path=rs.getServletContext().getRealPath("/resources/upload/review/");
+		File uploadDir=new File(path);
+		
+		if(!uploadDir.exists()) uploadDir.mkdirs();
+		
+		if(!upFile.isEmpty()) {
+			//리네임드처리하기
+			String originalFilename=upFile.getOriginalFilename();
+			String ext=originalFilename.substring(originalFilename.lastIndexOf("."));
+			//리네임 명칭을 정할값 설정
+			SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMdd_HHmmssSSS");
+			int rndNum=(int)(Math.random()*10000);
+			String rename=sdf.format(System.currentTimeMillis())+"_"+rndNum+ext;
+						
+			//업로드처리하기
+			try {
+				upFile.transferTo(new File(path+rename));
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		String msg="";
+		String loc="";
+		try {
+			service.reviewWrite(r);
+			msg="게시글입력성공";	
+			loc="/board/boardList.do";
+		}catch(RuntimeException e) {
+			msg="게시글입력실패";	
+			loc="/board/boardWrite.do";			
+		}
+		
+		m.addAttribute("msg",msg);
+	
+		return "common/msg";
+	}
 	
 }
