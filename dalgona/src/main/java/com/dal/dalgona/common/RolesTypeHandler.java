c4aa07d3d555e887bd8f1f5a65faf4e1194a06da
taera <taera2@icloud.com>
2022-08-16 12:18:43 +0900

    시큐리티제거

--- dalgona/src/main/java/com/dal/dalgona/common/RolesTypeHandler.java
+++ dalgona/src/main/java/com/dal/dalgona/common/RolesTypeHandler.java
@@ -1,49 +1,49 @@
-package com.dal.dalgona.common;
-
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.apache.ibatis.type.JdbcType;
-import org.apache.ibatis.type.TypeHandler;
-
-import com.dal.dalgona.common.model.vo.Roles;
-
-//public class RolesTypeHandler extends BaseTypeHandler<Roles> {
-public class RolesTypeHandler implements TypeHandler<Roles> {
-
-	@Override
-	public void setParameter(PreparedStatement ps, int i, Roles roles, JdbcType jdbcType) throws SQLException {
-		// TODO Auto-generated method stub
-		//System.out.println(roles.name());
-		if(roles != null) {
-			ps.setString(i, roles.name());
-		}else {
-			ps.setString(i, "USER");
-			// default 값이 정해져 있어 getKey() == NULL이 들어 올 수 없지만 분기처리를 위해 작성할 것
-		}
-		
-	}
-
-	@Override
-	public Roles getResult(ResultSet rs, String columnName) throws SQLException {
-		// TODO Auto-generated method stub
-
-		return Roles.valueOf(rs.getString(columnName));
-	}
-
-	@Override
-	public Roles getResult(ResultSet rs, int columnIndex) throws SQLException {
-		// TODO Auto-generated method stub
-
-		return Roles.valueOf(rs.getString(columnIndex));
-	}
-
-	@Override
-	public Roles getResult(CallableStatement cs, int columnIndex) throws SQLException {
-		// TODO Auto-generated method stub
-		return Roles.valueOf(cs.getString(columnIndex));
-	}
-	
-}
+//package com.dal.dalgona.common;
+//
+//import java.sql.CallableStatement;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//
+//import org.apache.ibatis.type.JdbcType;
+//import org.apache.ibatis.type.TypeHandler;
+//
+//import com.dal.dalgona.common.model.vo.Roles;
+//
+////public class RolesTypeHandler extends BaseTypeHandler<Roles> {
+//public class RolesTypeHandler implements TypeHandler<Roles> {
+//
+//	@Override
+//	public void setParameter(PreparedStatement ps, int i, Roles roles, JdbcType jdbcType) throws SQLException {
+//		// TODO Auto-generated method stub
+//		//System.out.println(roles.name());
+//		if(roles != null) {
+//			ps.setString(i, roles.name());
+//		}else {
+//			ps.setString(i, "USER");
+//			// default 값이 정해져 있어 getKey() == NULL이 들어 올 수 없지만 분기처리를 위해 작성할 것
+//		}
+//		
+//	}
+//
+//	@Override
+//	public Roles getResult(ResultSet rs, String columnName) throws SQLException {
+//		// TODO Auto-generated method stub
+//
+//		return Roles.valueOf(rs.getString(columnName));
+//	}
+//
+//	@Override
+//	public Roles getResult(ResultSet rs, int columnIndex) throws SQLException {
+//		// TODO Auto-generated method stub
+//
+//		return Roles.valueOf(rs.getString(columnIndex));
+//	}
+//
+//	@Override
+//	public Roles getResult(CallableStatement cs, int columnIndex) throws SQLException {
+//		// TODO Auto-generated method stub
+//		return Roles.valueOf(cs.getString(columnIndex));
+//	}
+//	
+//}
--- dalgona/src/main/java/com/dal/dalgona/common/model/vo/Roles.java
+++ dalgona/src/main/java/com/dal/dalgona/common/model/vo/Roles.java
@@ -1,23 +1,23 @@
-package com.dal.dalgona.common.model.vo;
-
-import lombok.Getter;
-
-@Getter
-public enum Roles {
-	
-	// spring security에서 ROLE_ 추적
-	ADMIN,
-	USER;
-	
-//	private final String key;
+//package com.dal.dalgona.common.model.vo;
 //
+//import lombok.Getter;
+//
+//@Getter
+//public enum Roles {
 //	
-//	Roles(String key) {
-//		this.key=key;
-//	}
+//	// spring security에서 ROLE_ 추적
+//	ADMIN,
+//	USER;
 //	
-//	public String getKey() {
-//		return key;
-//	}
-
-}
+////	private final String key;
+////
+////	
+////	Roles(String key) {
+////		this.key=key;
+////	}
+////	
+////	public String getKey() {
+////		return key;
+////	}
+//
+//}
