35ad198465ae121cc5bf949b00a1819aa01f9220
RyuWonHee <dnjs1@DESKTOP-6M4H485>
2022-08-18 17:29:40 +0900

    수정중

---
+++ dalgona/src/main/java/com/dal/dalgona/orderdetail/model/dao/OrderDetailDaoImpl.java
@@ -0,0 +1,19 @@
+package com.dal.dalgona.orderdetail.model.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.dal.dalgona.common.model.vo.OrderDetail;
+
+public class OrderDetailDaoImpl implements OrderDetailDao {
+
+//	@Override
+//	public List<OrderDetail> selectOrderDetail(SqlSessionTemplate session) {
+//		
+//		return session.selectList("orderDetail.orderDetailList");
+//	}
+
+	
+	
+}
